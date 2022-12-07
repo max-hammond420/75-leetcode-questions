eaa4f3631f5ff992983e0886c156201bb9fadaa0
Max Hammond <max.hammond21@gmail.com>
2022-12-08 09:52:49 +1100

    funky O(n^2) solution

--- minimum-size-subarray-sum/main.py
+++ minimum-size-subarray-sum/main.py
@@ -8,13 +8,25 @@ class Solution:
         i = 0
         j = 0
         minimum = sys.maxsize
-        sub_array = [nums[0]]
+        sub_array = []
 
-        while (i < n):
-            if sum(sub_array) >= target:
-                minimum = min(minimum, len(sub_array))
+        while (j < n):
+            if sum(sub_array) < target and i < n:
+                sub_array.append(nums[i])
                 i += 1
+            elif sum(sub_array) >= target:
+                minimum = min(minimum, len(sub_array))
+                sub_array.pop(0)
+                j += 1
             else:
-                pass
+                j += 1
+
+        if (minimum == sys.maxsize):
+            return 0
 
         return minimum
+
+t = [1, 4, 4]
+u = [2,3,1,2,4,3]
+v = [1,1,1,1,1,1,1,1]
+print(Solution.minSubArrayLen(Solution, 11, v))
