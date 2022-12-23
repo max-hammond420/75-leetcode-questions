c8fd1572a36e89e882938fce50543ef4079c2c4f
Max Hammond <max.hammond21@gmail.com>
2022-12-23 20:32:12 +1100

    Use a stack to check parenthesis are getting closed

---
+++ valid-parentheses/main.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        ls = []
+
+        for i in s:
+            if i in ['(', '[', '{']:
+                ls.append(i)
+            elif len(ls) == 0:
+                return False
+            elif i == ')' and ls[-1] == '(':
+                ls.pop()
+            elif i == '}' and ls[-1] == '{':
+                ls.pop()
+            elif i == ']' and ls[-1] == '[':
+                ls.pop()
+            else:
+                return False
+
+        if len(ls) == 0:
+            return True
+        return False
+
+print(Solution.isValid(Solution, '(])'))
