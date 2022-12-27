35160a07ba92a54826e3f884b93df1ddb8ad7e45
Max Hammond <max.hammond21@gmail.com>
2022-12-28 10:58:07 +1100

    consider discrete cases and update l, h pointer accordingly

---
+++ search-in-rotated-sorted-array/main.py
@@ -0,0 +1,25 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # left sorted portion
+            if nums[low] < nums[mid]:
+                if (target > nums[mid]) or (target < nums[low]):
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+            # right sorted portion
+            else:
+                if (target < nums[mid]) or target > nums[high]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+        return -1
