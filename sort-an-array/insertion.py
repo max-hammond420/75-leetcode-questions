dcc579a73583fa90aaef7e5ff327afc04c218896
Max Hammond <max.hammond21@gmail.com>
2023-01-02 21:01:57 +1100

    only fiddle with j pointer

---
+++ sort-an-array/insertion.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        # insertion sort
+        for i in range(1, len(nums)):
+            j = i - 1
+            while (j >= 0 and nums[j + 1] < nums[j]):
+                tmp = nums[j + 1]
+                nums[j+1] = nums[j]
+                nums[j] = tmp
+                j -= 1
+
+        return nums
