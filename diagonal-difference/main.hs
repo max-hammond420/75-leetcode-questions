f81e2068d2e3d1a99257c65462debe5287c0ba4c
Max Hammond <max.hammond21@gmail.com>
2023-01-05 16:27:08 +1100

    work in progresss

---
+++ diagonal-difference/main.hs
@@ -0,0 +1,12 @@
+solvr :: [Int] -> Int
+solvr x [y] = undefined
+
+solvl :: [Int] -> Int
+solvl x [y] = undefined
+
+solve :: [Int] -> int
+solve = undefined
+-- solve [x] = abs (solvr [x] - solvl [x])
+
+main :: IO ()
+main = undefined
