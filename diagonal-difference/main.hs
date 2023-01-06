d61c23fbe78a9214c4f154dae7bfbc3171daa083
Max Hammond <max.hammond21@gmail.com>
2023-01-06 22:58:51 +1100

    need to implement solve

--- diagonal-difference/main.hs
+++ diagonal-difference/main.hs
@@ -1,11 +1,14 @@
-solvr :: [Int] -> Int
-solvr x [y] = undefined
+import Data.List
 
-solvl :: [Int] -> Int
-solvl x [y] = undefined
+chop :: Int -> [Int] -> [[Int]]
+chop _ [] = []
+chop n xs = take n xs:chop n (drop n xs)
+
+-- sumDiagonal :: [Int] -> Int
+sumDiagonal x = sum $ zipWith (uncurry (!!)) [0..] $ chop 3 x
 
 solve :: [Int] -> int
-solve = undefined
+solve x = abs (sumDiagonal $ tail x - sumDiagonal $ transpose $ tail x)
 -- solve [x] = abs (solvr [x] - solvl [x])
 
 main :: IO ()
