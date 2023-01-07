352d98b813f10ed8f1b3e00cfa925e4e40e49c97
Max Hammond <max.hammond21@gmail.com>
2023-01-08 09:08:58 +1100

    progress

--- diagonal-difference/main.hs
+++ diagonal-difference/main.hs
@@ -4,11 +4,15 @@ chop :: Int -> [Int] -> [[Int]]
 chop _ [] = []
 chop n xs = take n xs:chop n (drop n xs)
 
--- sumDiagonal :: [Int] -> Int
-sumDiagonal x = sum $ zipWith (uncurry (!!)) [0..] $ chop 3 x
+sumDiagonal :: [[Int]] -> Int
+sumDiagonal x = sum $ map (uncurry (!!)) $ zip x [0..]
 
-solve :: [Int] -> int
-solve x = abs (sumDiagonal $ tail x - sumDiagonal $ transpose $ tail x)
+sumReverseDiagonal :: [[Int]] -> Int
+sumReverseDiagonal x = sum $ map (uncurry (!!)) $ zip (map reverse x) [0..]
+
+-- sumDiagnonal (tail x)
+solve :: [[Int]] -> int
+solve x = sumDiagonal x - sumReverseDiagonal x
 -- solve [x] = abs (solvr [x] - solvl [x])
 
 main :: IO ()
