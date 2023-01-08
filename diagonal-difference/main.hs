a5a0a9b090b1ffb6f941b6d29217d683c9842865
Max Hammond <max.hammond21@gmail.com>
2023-01-08 16:02:27 +1100

    list comprehension and functional composition

--- diagonal-difference/main.hs
+++ diagonal-difference/main.hs
@@ -7,13 +7,8 @@ chop n xs = take n xs:chop n (drop n xs)
 sumDiagonal :: [[Int]] -> Int
 sumDiagonal x = sum $ map (uncurry (!!)) $ zip x [0..]
 
-sumReverseDiagonal :: [[Int]] -> Int
-sumReverseDiagonal x = sum $ map (uncurry (!!)) $ zip (map reverse x) [0..]
-
--- sumDiagnonal (tail x)
-solve :: [[Int]] -> int
-solve x = sumDiagonal x - sumReverseDiagonal x
--- solve [x] = abs (solvr [x] - solvl [x])
+solve :: [Int] -> Int
+solve (x:xs) = abs $ sumDiagonal (chop x xs) - sumDiagonal (map reverse $ chop x xs)
 
 main :: IO ()
-main = undefined
+main = interact $ show . solve . map read . words
