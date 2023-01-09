4524c1781427a825bd6ceba94e68c32f32618114
Max Hammond <max.hammond21@gmail.com>
2023-01-09 13:28:25 +1100

    create global array, of elements in bst, then index it

---
+++ kth-smallest-element-in-a-bst/main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    vector<int> nums;
+    int kthSmallest(TreeNode* root, int k) {
+        
+        appendNums(root);
+        return nums[k];
+    }
+
+    void appendNums(TreeNode* root) {
+        if (!root) {
+            return;
+        }
+
+        appendNums(root->left);
+        nums.push_back(root->val);
+        appendNums(root->right);
+    }
+};
